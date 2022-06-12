c67ff2f09597d3e281ecdbe719565cf9b8fad79f
Pots <91101609+PotDeCannabis@users.noreply.github.com>
2022-06-12 22:02:42 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,256 @@
+ESX = nil
+ 
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+    end
+end)
+
+-- Intéraction [E]
+
+Citizen.CreateThread(function()
+    while true do
+        local wait = 900
+        for k,v in pairs(Config.Positions) do
+            local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
+            local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, v.x, v.y, v.z)
+            if dist <= 5 then 
+                wait = 1                                                 
+                DrawMarker(6, v.x, v.y, v.z-0.99, nil, nil, nil, -90, nil, nil, 1.0, 1.0, 1.0, 230, 230, 0 , 120)
+             end
+            if dist <= 2 then
+                wait = 1
+                Visual.Subtitle("Appuyer sur ~y~[E]~s~ pour accèder à ~y~l'armurerie ~s~!", 1) 
+                if IsControlJustPressed(1,51) then
+                    OpenArmurerie()
+                end
+            end 
+        end
+    Citizen.Wait(wait)
+    end
+end)
+
+-- Blips
+
+Citizen.CreateThread(function()
+    for k, v in pairs(Config.Positions) do
+        local pos = Config.Positions
+        local Blips = AddBlipForCoord(pos[k].x, pos[k].y, pos[k].z)
+        SetBlipSprite(Blips, 110)
+        SetBlipColour(Blips, 5)
+        SetBlipScale(Blips, 0.7)
+        SetBlipAsShortRange(Blips, true)
+        BeginTextCommandSetBlipName('STRING')
+        AddTextComponentString("Armurerie")
+        EndTextCommandSetBlipName(Blips)
+    Citizen.Wait(1)
+    end
+end)
+
+-- NPC
+
+CreateThread(function()
+    local canCallJob = false
+    for k, v in pairs(Config.NPC) do
+        RequestModel(GetHashKey(v.typePed))
+        while not HasModelLoaded(GetHashKey(v.typePed)) do
+            Wait(1)
+        end
+        local npc = CreatePed(4, v.typePed, v.position.x, v.position.y, v.position.z-0.98, v.heading,  false, true)
+        SetPedFleeAttributes(npc, 0, 0)
+        SetPedDropsWeaponsWhenDead(npc, false)
+        SetPedDiesWhenInjured(npc, false)
+        SetEntityInvincible(npc , true)
+        FreezeEntityPosition(npc, true)
+        SetBlockingOfNonTemporaryEvents(npc, true)
+    end
+end)
+
+-- Vérification PPA
+
+function VerifPPA()
+    ESX.TriggerServerCallback('esx_license:checkLicense', function(hasWeaponLicense)
+        possessionppa = hasWeaponLicense
+    end, GetPlayerServerId(PlayerId()), 'weapon')
+end 
+
+-- Menu
+
+local open = false 
+local mainMenu = RageUI.CreateMenu('Armurerie', 'Moyens de payement')
+local mainMenu2 = RageUI.CreateSubMenu(mainMenu, 'Armurerie', 'Actions armurerie')
+local mainMenu3 = RageUI.CreateSubMenu(mainMenu2, 'Armurerie', 'Actions armurerie')
+local mainMenu4 = RageUI.CreateSubMenu(mainMenu2, 'Armurerie', 'Actions armurerie')
+local mainMenu5 = RageUI.CreateSubMenu(mainMenu, 'Armurerie', 'Actions armurerie')
+local mainMenu6 = RageUI.CreateSubMenu(mainMenu5, 'Armurerie', 'Actions armurerie')
+local mainMenu7 = RageUI.CreateSubMenu(mainMenu5, 'Armurerie', 'Actions armurerie')
+mainMenu.Display.Header = true 
+mainMenu.Closed = function()
+  FreezeEntityPosition(PlayerPedId(), false)
+  open = false
+end
+
+function OpenArmurerie()
+    if open then 
+        open = false
+        RageUI.Visible(mainMenu, false)
+        return
+    else
+        open = true 
+        RageUI.Visible(mainMenu, true)
+        VerifPPA()
+        CreateThread(function()
+            while open do
+                if Config.PPAObligatoire == true then
+                    if not possessionppa then
+                        RageUI.IsVisible(mainMenu, function() 
+                          
+                            RageUI.Separator("↓ ~y~ Vous n'avez pas le PPA ~s~↓")
+
+                            RageUI.Button("Liquide", nil, {RightLabel = "→"}, false , {
+                            onSelected = function() 
+                            end
+                            })
+
+                            RageUI.Button("Banque", nil, {RightLabel = "→"}, false , {
+                            onSelected = function() 
+                            end
+                            })
+                        end)
+                    else
+                        RageUI.IsVisible(mainMenu, function() 
+                          
+                            RageUI.Separator("↓ ~y~ Armurerie ~s~↓")
+
+                            RageUI.Button("Liquide", nil, {RightLabel = "→"}, true , {
+                            onSelected = function() 
+                            end
+                            }, mainMenu2)
+
+                            RageUI.Button("Banque", nil, {RightLabel = "→"}, true , {
+                            onSelected = function() 
+                            end
+                            }, mainMenu5)
+
+                        end)
+                    end
+                else
+                    RageUI.IsVisible(mainMenu, function() 
+                          
+                        RageUI.Separator("↓ ~y~ Armurerie ~s~↓")
+
+                        RageUI.Button("Liquide", nil, {RightLabel = "→"}, true , {
+                        onSelected = function() 
+                        end
+                        }, mainMenu2)
+
+                        RageUI.Button("Banque", nil, {RightLabel = "→"}, true , {
+                        onSelected = function() 
+                        end
+                        }, mainMenu5)
+
+                    end)
+                end
+
+                RageUI.IsVisible(mainMenu2, function() 
+  
+                    RageUI.Separator("→ Mode de Paiement : ~y~Liquide ~s~ ←")
+
+                    RageUI.Button("Armes Blanches", nil, {RightLabel = "→"}, true , {
+                    onSelected = function() 
+                    end
+                    }, mainMenu3)
+
+                    RageUI.Button("Armes à feu", nil, {RightLabel = "→"}, true , {
+                    onSelected = function() 
+                    end
+                    }, mainMenu4)
+
+                end)
+
+                RageUI.IsVisible(mainMenu3, function() 
+
+                    RageUI.Button("Knife", nil, {RightLabel = "~y~50 $"}, true , {
+                    onSelected = function()
+                        TriggerServerEvent('pAmunation:achatLiquideKnife')
+                    end
+                    })
+
+                    RageUI.Button("Machette", nil, {RightLabel = "~y~35 $"}, true , {
+                    onSelected = function() 
+                        TriggerServerEvent('pAmunation:achatLiquideMachette')
+                    end
+                    })
+
+                end)
+
+                RageUI.IsVisible(mainMenu4, function() 
+
+                    RageUI.Button("Pistolet de combat", nil, {RightLabel = "~y~1200 $"}, true , {
+                    onSelected = function()
+                        TriggerServerEvent('pAmunation:achatLiquidePistoletCombat')
+                    end
+                    })
+
+                    RageUI.Button("Pistolet SNS", nil, {RightLabel = "~y~800 $"}, true , {
+                    onSelected = function() 
+                        TriggerServerEvent('pAmunation:achatLiquidePistoletSNS')
+                    end
+                    })
+
+                end)
+
+                RageUI.IsVisible(mainMenu5, function() 
+                    
+                    RageUI.Separator("→ Mode de Paiement : ~y~Banque ~s~ ←")
+
+                    RageUI.Button("Armes Blanches", nil, {RightLabel = "→"}, true , {
+                    onSelected = function() 
+                    end
+                    }, mainMenu6)
+
+                    RageUI.Button("Armes à feu", nil, {RightLabel = "→"}, true , {
+                    onSelected = function() 
+                    end
+                    }, mainMenu7)
+
+                end)
+
+                RageUI.IsVisible(mainMenu6, function() 
+
+                    RageUI.Button("Knife", nil, {RightLabel = "~y~50 $"}, true , {
+                    onSelected = function()
+                        TriggerServerEvent('pAmunation:achatBanqueKnife')
+                    end
+                    })
+
+                    RageUI.Button("Machette", nil, {RightLabel = "~y~35 $"}, true , {
+                    onSelected = function() 
+                        TriggerServerEvent('pAmunation:achatBanqueMachette')
+                    end
+                    })
+
+                end)
+
+                RageUI.IsVisible(mainMenu7, function() 
+
+                    RageUI.Button("Pistolet de combat", nil, {RightLabel = "~y~1200 $"}, true , {
+                    onSelected = function() 
+                        TriggerServerEvent('pAmunation:achatBanquePistolet')
+                    end
+                    })
+
+                    RageUI.Button("Pistolet SNS", nil, {RightLabel = "~y~800 $"}, true , {
+                    onSelected = function() 
+                        TriggerServerEvent('pAmunation:achatBanquePistoletSNS')
+                    end
+                    })
+
+                end)
+
+                Wait(0)
+            end
+        end)
+    end
+end
---
+++ config.lua
@@ -0,0 +1,28 @@
+Config = {}
+
+Config = {
+
+	Positions = {
+		{x = 21.57, y = -1106.89, z = 29.79},
+		{x = 810.2, y = -2157.3, z = 29.60},
+		{x = 1693.41, y = 3759.53, z = 34.73},
+		{x = -330.23, y = 6083.84, z = 31.43},
+		{x = 252.34, y = -50.01, z = 69.91},
+		{x = 2567.69, y = 294.35, z = 108.77},
+		{x = -1117.56, y = 2698.68, z = 18.53},
+		{x = 842.46, y = -1033.49, z = 28.17}
+	},
+
+	NPC = {
+		{position = vec3(22.06, -1105.17, 29.79), heading = 160.39, typePed = "s_m_m_cntrybar_01"},
+		{position = vec3(810.26, -2159.09, 29.61), heading = 2.28, typePed = "s_m_m_cntrybar_01"},
+		{position = vec3(1691.81, 3760.85, 34.70), heading = 224.77, typePed = "s_m_m_cntrybar_01"},
+		{position = vec3(-331.62, 6085.11, 31.45), heading = 221.81, typePed = "s_m_m_cntrybar_01"},
+		{position = vec3(254.01, -50.52, 69.94), heading = 73.20, typePed = "s_m_m_cntrybar_01"},
+		{position = vec3(2567.71, 292.39, 108.73), heading = 4.87, typePed = "s_m_m_cntrybar_01"},
+		{position = vec3(-1118.94, 2700.05, 18.55), heading = 219.91, typePed = "s_m_m_cntrybar_01"},
+		{position = vec3(842.54, -1035.43, 28.19), heading = 2.73, typePed = "s_m_m_cntrybar_01"},
+	},
+
+	PPAObligatoire = false
+}
